6996262f235804ba729c8f43b166c6a118d93a2a
NguyenHung <hungkill146@gmail.com>
2026-09-19 16:17:46 +0700

    sửa database 2

--- data/db_LexiLoop.sql
+++ data/db_LexiLoop.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hoc_ngoai_ngu`
+-- Database: `db_LexLoop  `
 --
 CREATE DATABASE IF NOT EXISTS `db_LexiLoop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `db_LexiLoop`;
